2b55cd6f1b5b99c9620a892c25db20bd82e3e3b8
dvdrtrgn <dvdrtrgn@gmail.com>
2014-08-25 10:39:53 -0500

    subsume the print css

--- tasks/options/sass.js
+++ tasks/options/sass.js
@@ -8,7 +8,6 @@ module.exports = {
             style: 'expanded', // cssmin will minify later
         },
         files: {
-            'app/build/print.css': 'scss/print.scss',
             'app/build/screen.css': 'scss/screen.scss',
         }
     }
