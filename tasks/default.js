cc9a411d3e3cface46814b921e68973b10d04b90
dvdrtrgn <dvdrtrgn@gmail.com>
2014-08-25 13:07:07 -0500

    compile wip

--- tasks/default.js
+++ tasks/default.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
     grunt.registerTask('default',
-        ['connect', 'jshint', 'concat', 'sass', 'watch']
-        /* 'imagemin', 'sass', 'uglify' */
+        ['connect', 'jshint', 'concat', 'sass', 'uglify', 'watch']
+        /* 'imagemin', 'sass' */
     );
 };
